34f84997d325e31b875ebfc555f043e4152a6026
EclipseGSX023 <eclipsegsx023@gmail.com>
2015-12-26 14:14:39 +0100

    overtime hardway

---
+++ ex22.py
@@ -0,0 +1,31 @@
+#print = print something
+
+#+ = plus
+
+#- = minus
+
+#/ = slash
+
+#* = asterisk
+
+#% = percent
+
+#< = less-than
+
+#> = greater-than
+
+#<= = less-than-equal
+
+#>= = greater-than-equal
+
+#" = double-quotes
+
+#' = single-quotes
+
+#% = percent
+
+#( ) = parenthesis
+
+#, = commas
+
+#\n = backslash n
